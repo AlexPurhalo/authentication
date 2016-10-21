e8c360ce616c8a2ab89c542a34141993826784ea
AlexPurhalo <alexpurhalo@gmail.com>
2016-10-21 12:59:54 +0300

    posts-scaffolding

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :destroy]
+
+  def index
+    @posts = Post.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
