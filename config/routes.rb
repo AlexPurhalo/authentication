9e7a95b0d5253f7220b8ae83220623ab8ec1e895
AlexPurhalo <alexpurhalo@gmail.com>
2016-10-21 14:30:42 +0300

    user-sign-up-ability

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts, only: [:show, :index, :create, :destroy, :new]
+
+  get 'signup' => 'users#new'
+
+  resources :users
 end
